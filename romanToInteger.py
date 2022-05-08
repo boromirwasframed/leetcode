f48a3f0a03a32734aa28ca2e3482f95829fe9479
boromirwasframed <92706482+boromirwasframed@users.noreply.github.com>
2022-05-08 09:45:42 -0700

    Update romanToInteger.py
    Optimized solution and moved dictionary outside of function definition.

--- romanToInteger.py
+++ romanToInteger.py
@@ -1,32 +1,28 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        numeralDict = {
-            'I'  : 1,
-            'IV' : 4,
-            'V'  : 5,
-            'IX' : 9,
-            'X'  : 10,
-            'XL' : 40,
-            'L'  : 50,
-            'XC' : 90,
-            'C'  : 100,
-            'CD' : 400,
-            'D'  : 500,
-            'CM' : 900,
-            'M'  : 1000
-        }
-        
+numeralDict = {
+    'I'  : 1,
+    'IV' : 4,
+    'V'  : 5,
+    'IX' : 9,
+    'X'  : 10,
+    'XL' : 40,
+    'L'  : 50,
+    'XC' : 90,
+    'C'  : 100,
+    'CD' : 400,
+    'D'  : 500,
+    'CM' : 900,
+    'M'  : 1000
+}
+
+class Solution:    
+    def romanToInt(self, s: str) -> int:        
         value = 0
         i = 0
         
         while i < len(s):
-            if i + 1 < len(s):
-                if s[i] + s[i + 1] in numeralDict:
-                    value += numeralDict[s[i] + s[i + 1]]
-                    i += 2
-                else:
-                    value += numeralDict[s[i]]
-                    i += 1
+            if i + 1 < len(s) and s[i] + s[i + 1] in numeralDict:
+                value += numeralDict[s[i] + s[i + 1]]
+                i += 2
             else:
                 value += numeralDict[s[i]]
                 i += 1
